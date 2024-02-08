91550512f5faed097bba99ebf09dfaafbfa43088
Iliyan Galabov <milenov556@gmail.com>
2024-02-08 17:38:37 +0200

    URL for get_book view added

--- BookLibrary/BookLibraryAPI/urls.py
+++ BookLibrary/BookLibraryAPI/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from BookLibrary.BookLibraryAPI.views import get_data, add_book
+from BookLibrary.BookLibraryAPI.views import get_data, add_book, get_book
 
 urlpatterns = [
     path('', get_data),
     path('add/', add_book),
+    path('get_book/<int:pk>', get_book)
 ]
