b12fa2e930ec98263d3f03f253d1f47ff9e94bba
Iliyan Galabov <milenov556@gmail.com>
2024-02-08 18:04:35 +0200

    URL for delete_book view added

--- BookLibrary/BookLibraryAPI/urls.py
+++ BookLibrary/BookLibraryAPI/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from BookLibrary.BookLibraryAPI.views import get_data, add_book, get_book, update_book
+from BookLibrary.BookLibraryAPI.views import get_data, add_book, get_book, update_book, delete_book
 
 urlpatterns = [
     path('', get_data, name='get data'),
     path('add-book/', add_book, name='add book'),
     path('get-book/<int:pk>', get_book, name='get book'),
-    path('update-book/<int:pk>', update_book, name='update book')
+    path('update-book/<int:pk>', update_book, name='update book'),
+    path('delete-book/<int:pk>', delete_book, name='delete book')
 ]
