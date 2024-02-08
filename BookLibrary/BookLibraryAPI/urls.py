8dc5e6cb91f9fd420ec7ede541ad5dfd2bda0b81
Iliyan Galabov <milenov556@gmail.com>
2024-02-08 17:49:43 +0200

    Names added to the URLs and minor changes to the paths

--- BookLibrary/BookLibraryAPI/urls.py
+++ BookLibrary/BookLibraryAPI/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from BookLibrary.BookLibraryAPI.views import get_data, add_book, get_book
 
 urlpatterns = [
-    path('', get_data),
-    path('add/', add_book),
-    path('get_book/<int:pk>', get_book)
+    path('', get_data, name='get data'),
+    path('add-book/', add_book, name='add book'),
+    path('get-book/<int:pk>', get_book, name='get book')
 ]
