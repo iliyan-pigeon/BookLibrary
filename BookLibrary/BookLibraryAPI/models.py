f455766b2d27dd8afe2af4977180b1bb61ae5d76
Iliyan Galabov <milenov556@gmail.com>
2024-02-12 17:52:26 +0200

    description field added to the Books model and some parameters to the other fields

--- BookLibrary/BookLibraryAPI/models.py
+++ BookLibrary/BookLibraryAPI/models.py
@@ -3,13 +3,23 @@ from django.db import models
 
 class Books(models.Model):
     headline = models.CharField(
-        max_length=200
+        max_length=200,
+        null=False,
+        blank=False,
     )
     author = models.CharField(
-        max_length=200
+        max_length=200,
+        null=False,
+        blank=False,
     )
     amount_in_stock = models.PositiveIntegerField(
         default=1,
+        null=False,
+        blank=False,
+    )
+    description = models.CharField(
+        max_length=500,
+        default="None",
         null=False,
         blank=False,
     )
