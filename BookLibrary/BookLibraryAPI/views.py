4ee553029e4edba29c60d647283f15924841170e
Iliyan Galabov <milenov556@gmail.com>
2024-02-12 18:55:45 +0200

    search_books view added

--- BookLibrary/BookLibraryAPI/views.py
+++ BookLibrary/BookLibraryAPI/views.py
@@ -1,4 +1,5 @@
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.db.models import Q
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from BookLibrary.BookLibraryAPI.serializers import BooksSerializer
@@ -56,3 +57,20 @@ def delete_book(request, pk):
     book.delete()
 
     return Response(f'The "{book.headline}" book is deleted!')
+
+
+@api_view(['GET'])
+def search_books(request):
+    search_query = request.query_params.get('search', None)
+
+    if search_query:
+        queryset = Books.objects.filter(
+            Q(headline__icontains=search_query) |
+            Q(author__icontains=search_query) |
+            Q(description__icontains=search_query)
+        )
+    else:
+        queryset = Books.objects.all()
+
+    serializer = BooksSerializer(queryset, many=True)
+    return Response(serializer.data)
