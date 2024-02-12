c6e26ed4db87f9d78d29c3af6fcd5c59bea07680
Iliyan Galabov <milenov556@gmail.com>
2024-02-12 17:21:06 +0200

    field for amount in stock added to the Books model

--- BookLibrary/BookLibraryAPI/models.py
+++ BookLibrary/BookLibraryAPI/models.py
@@ -8,3 +8,8 @@ class Books(models.Model):
     author = models.CharField(
         max_length=200
     )
+    amount_in_stock = models.PositiveIntegerField(
+        default=1,
+        null=False,
+        blank=False,
+    )
