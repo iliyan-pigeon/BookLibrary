7c8a77bf6d04865b562cf3c0e6eda066647c3608
Iliyan Galabov <milenov556@gmail.com>
2024-02-14 10:19:26 +0200

    BooksUserSerializer added to the file

--- BookLibrary/BookLibraryAPI/serializers.py
+++ BookLibrary/BookLibraryAPI/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
-from BookLibrary.BookLibraryAPI.models import Books
+from BookLibrary.BookLibraryAPI.models import Books, BooksUser
 
 
 class BooksSerializer(serializers.ModelSerializer):
     class Meta:
         model = Books
         fields = '__all__'
+
+
+class BooksUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BooksUser
+        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'gender', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
