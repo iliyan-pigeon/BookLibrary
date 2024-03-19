d7cfe1f73792d67772ab6e170b3c370c00b95558
Iliyan Galabov <milenov556@gmail.com>
2024-03-19 10:33:41 +0200

    Deleted unnecessary structure

--- BookLibrary/BookLibraryFrontend/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class BooklibraryfrontendConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'BookLibrary.BookLibraryFrontend'
--- BookLibrary/BookLibraryFrontend/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from BookLibrary.BookLibraryFrontend.views import index
-
-urlpatterns = [
-    path('', index, name='index')
-]
